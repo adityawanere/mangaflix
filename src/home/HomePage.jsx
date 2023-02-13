f7ac9e4a18bd6247192499f85525fa0293371c42
Aditya Wanere <aditya.wanere2000@gmail.com>
2023-02-13 23:39:38 +0530

    Added list and listItem

--- src/home/HomePage.jsx
+++ src/home/HomePage.jsx
@@ -1,12 +1,18 @@
 import Featured from "../components/featured/Featured";
 import Navbar from "../components/navbar/Navbar";
+import List from "../components/list/List";
 import "./home.scss";
 
 const home = () => {
   return (
     <div className="home">
       <Navbar />
-      <Featured type="movie" />
+      <Featured />
+      <List />
+      <List />
+      <List />
+      <List />
+      <List />
     </div>
   );
 };
