63e1dbdf23777248e071d93d961b00e7ee88c237
Aditya Wanere <aditya.wanere2000@gmail.com>
2023-07-23 15:32:47 +0530

    Fixes

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -33,7 +33,7 @@ const ListItem = ({ index }) => {
       style={{
         left: isHovered && index * 225 - 50 + index * 2.5,
         width: isHovered && "325px",
-        height: isHovered && "300px",
+        height: isHovered && "310px",
         top: isHovered && "-150px",
         borderRadius: isHovered && "5px",
         position: isHovered && "absolute",
@@ -43,7 +43,7 @@ const ListItem = ({ index }) => {
       <img
         src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
         alt=""
-        style={{ height: isHovered && "180px" }}
+        style={{ height: isHovered && "140px" }}
       />
       {isHovered && (
         <>
@@ -59,10 +59,10 @@ const ListItem = ({ index }) => {
               <span className="limit">U/A +16</span>
               <span>1999</span>
             </div>
-            {/* <div className="desc">
+            <div className="desc">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit.
               Praesentium hic rem eveniet error possimus, neque ex doloribus.
-            </div> */}
+            </div>
             <div className="genre">Action </div>
           </div>
         </>
